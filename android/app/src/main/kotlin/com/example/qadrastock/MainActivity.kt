053cc5df44c832fb9d25717925a8cfea5c3532c7
1304Gallo <alex980413@gmail.com>
2025-12-19 20:02:14 -0800

    Creando la app

---
+++ android/app/src/main/kotlin/com/example/qadrastock/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.qadrastock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/app_color.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color primary = Color(0xffF2F2F2);
+  static const Color secondary = Color(0xff04ADBF);
+  static const Color accent = Color(0xffF2A30F);
+
+  // Backgrounds
+  static const Color background = Color(0xff3B4953);
+  static const Color backgroundComponent = Color(0xff58697c);
+  static const Color backgroundComponentSelected = Color(0xff0D0409);
+}
